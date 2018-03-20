4be515a2045a06fc01c39d01ce4a8f7c3b1b5b6a
ryssuke <xx-jm@live.com>
2018-03-20 17:03:37 +0800

    Login/register without database...orz

---
+++ app/src/main/java/com/androb/androidrobot/CollegeStuMainActivity.java
@@ -0,0 +1,18 @@
+package com.androb.androidrobot;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kaki on 2017/03/20.
+ */
+
+public class CollegeStuMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.collegestumain_layout);
+    }
+
+}
--- app/src/main/java/com/androb/androidrobot/MainActivity.java
+++ app/src/main/java/com/androb/androidrobot/MainActivity.java
@@ -1,7 +1,15 @@
 package com.androb.androidrobot;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        ButterKnife.bind(this);
     }
 }
